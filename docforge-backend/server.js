c6241cc538884b05a6c57d048c6a1ceb2f86fb5f
Aditya Verma <av905666@gmail.com>
2026-04-16 18:32:41 +0530

    .

--- docforge-backend/server.js
+++ docforge-backend/server.js
@@ -1,5 +1,12 @@
 const app = require("./src/app");
+const cors = require("cors");
 const PORT=5000;
+
+app.use(
+  cors({
+    origin: "https://doc-forge-one.vercel.app",
+  })
+);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
